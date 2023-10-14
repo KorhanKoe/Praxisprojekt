620a34a54ee66eb1b8de112ffcb1622851768e4d
Hasan <131282178+HasanKoe@users.noreply.github.com>
2023-10-14 13:31:19 +0200

    footer.php
    footer-Datei.

---
+++ footer.php
@@ -0,0 +1,36 @@
+<footer class="footer">
+    <div class="container">
+      <div class="row">
+        <div class="footer-col">
+          <h4>company</h4>
+          <ul>
+            <li><a href="#">about us</a></li>
+            <li><a href="#">our services</a></li>
+            <li><a href="#">privacy policy</a></li>
+            <li><a href="#">affiliate program</a></li>
+          </ul>     
+        </div>
+        <div class="footer-col">
+          <h4>get help</h4>
+          <ul>
+            <li><a href="#">FAQ</a></li>
+            <li><a href="#">Reviews</a></li>
+            <li><a href="#">Start</a></li>
+            <li><a href="#">Kontakt</a></li>
+            <li><a href="#">Jobs</a></li>
+          </ul>     
+        </div>
+        <div class="footer-col">
+          <h4>follow us</h4>
+          <div class="social-links">
+            <a href="#"><i class="fab fa-github"></i></a>
+            <a href="#"><i class="fab fa-xing"></i></a>
+            <a href="#"><i class="fab fa-instagram"></i></a>
+            <a href="#"><i class="fab fa-linkedin-in"></i></a>
+    </div>
+ 
+  </footer>        
+
+  <?php wp_footer(); ?>
+</body>
+</html>
