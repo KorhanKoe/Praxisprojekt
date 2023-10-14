6a6910387418495cd28db55472363392ab724e03
Hasan <131282178+HasanKoe@users.noreply.github.com>
2023-10-14 13:30:57 +0200

    header.php
    Header-Datei.

---
+++ header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/15d7c529f1.js" crossorigin="anonymous"></script>
+
+    <?php wp_head(); ?>
+    <title>tveds - Digital Agency</title>
+    
+    <!-- ?php echo get_template_directory_uri(); ?> -->
+
+  </head>
