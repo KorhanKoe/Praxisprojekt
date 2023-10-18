3236bf6775207f29e807d0be98e608c79879121a
Hasan <131282178+HasanKoe@users.noreply.github.com>
2023-10-18 22:24:26 +0200

    Update funcitons.php

--- funcitons.php
+++ funcitons.php
@@ -1,4 +1,5 @@
 <?php
+// adding the CSS and JS filesize
 //2. parameter zum übernehmen des Stylesheets, 3. Die Dependencies, 4. Version Nummer, 5.
 //Versionnummer muss bei jeder Änderung actualisiert werden, es kann stattdessen microtime() während der Entwicklung genutzt werden, was die Versions-Nummer bei jedem Aufruf ändert.
 function tv_setup() {
@@ -14,13 +15,15 @@ add_action('wp_enqueue_scripts', 'tv_setup');       //Hier wird festgelegt, wann
 // html theme support
 function tv_init() {
     add_theme_support('post-thumbnails');
-    add_theme_support('title-tag');
+    add_theme_support('title-tag');      // für die Tab Schrift
     add_theme_support('html5',
-        array('comment-list', 'comment-form','search-form')
+        array('comment-list', 'comment-form','search-form')     //hier soll html5 implementiert werden
     );
 }
 
 add_action('after-setup_theme', 'gt-init');
+
+//projects post type
 
 function tv_custom_post_type() {
     register_post_type('project',
@@ -34,7 +37,7 @@ function tv_custom_post_type() {
             ),
             'menu-icon' => 'dashicons-clipboard',
             'public' => true,
-            'has_archive' => true,
+            'has_archive' => true,      //ermöglicht das Nutzen von archives
             'supports' => array(
                 'title', 'thumbnail', 'editor', 'excerpt', 'comments'
             )
@@ -42,5 +45,25 @@ function tv_custom_post_type() {
             );
 }
 
-add_action('init', 'tv_custom_post_type');
+add_action('init', 'tv_custom_post_type');      //um die Function zu starten
 
+if(isset($_POST['submitbtn'])) {        //checked den submit button
+    $data = array(                      //falls ja, werden diese Dateien abgerufen
+        'vorname' => $_POST['vorname'],
+        'nachname' => $_POST['nachname'],
+        'email' => $_POST['email'],
+        'info' => $_POST['info'],
+    );
+
+    $table_name ='kundentv';           //ziel Tabell in der Datenbank, die die Daten bekommt
+
+    $result = $wpdb->insert($table_name, $data, $format=NULL);
+
+    if ($result==1) {
+        echo "<script>alert('Sie haben sich erfolgreich registriert! Checken Sie Ihren Mail-Ordner');</script>";
+    }else{
+        echo "<script>alert('Keine Registrierung möglich.');</script>";
+        }
+    }
+
+?>
