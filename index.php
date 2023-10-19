247e86dd7313043af59f6b1bf0fb8e2f16f541c0
Hasan <131282178+HasanKoe@users.noreply.github.com>
2023-10-19 16:12:16 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
-Sie haben sich erfolgreich registriert.
+<?php get_header(); ?>
+<section id="top">
+
+</section>
+<a href="<?php echo home_url(); ?>"> 
+<img 
+    src="<?php echo get_template_directory_uri(); ?>/bildr/tveds.png"
+    alt="Logo tveds.com"/>
+    </a>
+
+<h3 id=txt7>Sie haben sich erfolgreich registriert.<br>
+Sie erhalten in wenigen Minuten eine Nachricht an Ihre Mail-Adresse mit dem Passwort für den Online Zugang.
+</h3>
+
