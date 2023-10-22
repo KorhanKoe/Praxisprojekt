d030033561cc378042998e1e9f1353fb06893746
Hasan <131282178+HasanKoe@users.noreply.github.com>
2023-10-22 12:25:01 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
+<!--Diese Seite erscheint, wenn der Kunde sich erfolgreich registriert hat. Der Grund für den Namen index.php ist die Hierarchie, in der Wordpress arbeitet. Die Verlinkung geschieht an dieser Stelle auf die default seite, nicht auf die Homepage, welche etwas anders aussieht. -->
+<!-- Über diese Default Seite kann dann zurück auf die Homepage gelangt werden -->
 <?php get_header(); ?>
-<section id="top">
+<section id="top">      <!--Für den Balken oben -->
 
 </section>
-<a href="<?php echo home_url(); ?>"> 
+<a href="<?php echo home_url(); ?>">    <!-- //Verlinkung zur Hauptseite nach Registrierung -->
 <img 
-    src="<?php echo get_template_directory_uri(); ?>/bildr/tveds.png"
+    src="<?php echo get_template_directory_uri(); ?>/bildr/tveds.png"       
     alt="Logo tveds.com"/>
     </a>
 
