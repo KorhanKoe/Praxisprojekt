6afd72a054f8211afc07da68c3587f4dd6c3d60e
Korhan <131282178+KorhanKoe@users.noreply.github.com>
2024-05-24 19:44:05 +0200

    Update header.php

--- header.php
+++ header.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8">    <!--Zeichencodierung -->
     <meta http-equiv="X-UA-Compatible" content="IE=edge">   <!--Bezieht sich auf internet explorer und zwingt es, die aktuellste version zu verwenden -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">   <!--  verändert die größe für Handy-Ansichten -->
-    <script src="https://kit.fontawesome.com/15d7c529f1.js" crossorigin="anonymous"></script> <!--fonts definieren die schriftart des Textes. Das ist der Link zu den fonts die ichv verwendet habe. -->
+    <script src="https://kit.fontawesome.com/15d7c529f1.js" crossorigin="anonymous"></script> <!--fonts definieren die Schriftart des Textes. Das ist der Link zu den Fonts die ich verwendet habe. -->
 
-    <?php wp_head(); ?>       <!--WordPress Funktion um den header dann in anderen dateien aufzurufen -->
+    <?php wp_head(); ?>       <!--WordPress Funktion um den header in anderen Dateien aufzurufen -->
     <title>tveds - Digital Agency</title>     <!--Titel der Seite im Browser, wird aber in WordPress nicht angezeigt. Wurde von HTML mit übernommen. -->
     
 
